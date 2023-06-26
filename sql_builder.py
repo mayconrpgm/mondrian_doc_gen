3a59887298cc4d72dc40eb68e778cc88246a374c
Maycon Queiros <maycon.queiros@bemobi.com.br>
2023-06-26 19:01:07 -0300

    Fix mutable default argument bug. Include visibility flag for dimension attributes

--- sql_builder.py
+++ sql_builder.py
@@ -5,13 +5,13 @@ import argparse
 import sys
 import os
 
-
 class DimensionLevel(object):
     name = ''
     caption = ''
     type = ''
     description = ''
     column = ''
+    is_visible = True
 
 class Dimension(object):
     id = ''
@@ -24,6 +24,10 @@ class Dimension(object):
     is_degenerated = False
     levels: list[DimensionLevel] = None
 
+    @property
+    def table_full_name(self):
+        return self.schema + '.' + self.table if self.schema != '' else self.table
+
     def __init__(self) -> None:
         id = ''
         name = ''
@@ -34,9 +38,6 @@ class Dimension(object):
         description = ''
         is_degenerated = False
         levels: list[DimensionLevel] = None
-
-    def table_full_name(self):
-        return self.schema + '.' + self.table if self.schema != '' else self.table
     
     def add_level(self, level: DimensionLevel):
         if self.levels is None:
@@ -61,14 +62,18 @@ class Measure(object):
     is_visible = True
 
 class Cube(object):
-    id = ''
+    id: str = ''
     table = ''
     schema = ''
     caption = ''
     description = ''
-    dimensions_usage: list[DimensionUsage] = []
-    measures: list[Measure] = []
+    dimensions_usage: list[DimensionUsage] = None
+    measures: list[Measure] = None
     aliases: dict[str, str] = {}
+
+    @property
+    def table_full_name(self):
+        return self.schema + '.' + self.table if self.schema != '' else self.table
 
     def __build_table_alias(self, table_name: str):
         first_letters = [word[0] for word in table_name.split('_')]
@@ -83,9 +88,18 @@ class Cube(object):
         self.aliases[alias] = table_name
 
         return alias
+
+    def add_dimension_usage(self, dim_usage: DimensionUsage):
+        if self.dimensions_usage is None:
+            self.dimensions_usage = []
+
+        self.dimensions_usage.append(dim_usage)
     
-    def table_full_name(self):
-        return self.schema + '.' + self.table if self.schema != '' else self.table
+    def add_measure(self, measure: Measure):
+        if self.measures is None:
+            self.measures = []
+
+        self.measures.append(measure)
 
     def build_sql_select(self):
         sql_string_columns = []
@@ -95,7 +109,7 @@ class Cube(object):
 
         fact_alias = self.__build_table_alias(self.table)
 
-        sql_string_from = "FROM\n\t{} {}".format(self.table_full_name(), fact_alias)
+        sql_string_from = "FROM\n    {} {}".format(self.table_full_name, fact_alias)
         group_by_count = 0
 
         for dim_usage in self.dimensions_usage:
@@ -103,11 +117,12 @@ class Cube(object):
             dim_alias = self.__build_table_alias(dim.table)
 
             if not dim.is_degenerated:
-                sql_string_join += '\nJOIN\n\t{} {} ON \n\t\t{}.{} = {}.{}'.format(dim.table_full_name(), dim_alias, fact_alias, dim_usage.foreign_key.lower(), dim_alias, dim.primary_key.lower())
+                sql_string_join += '\nJOIN\n    {} {} ON \n        {}.{} = {}.{}'.format(dim.table_full_name, dim_alias, dim_alias, dim.primary_key.lower(), fact_alias, dim_usage.foreign_key.lower())
 
             for level in dim.levels:
-                sql_string_columns.append('{}.{} '.format(dim_alias, level.column.lower()))
-                group_by_count += 1
+                if level.is_visible:
+                    sql_string_columns.append('{}.{} '.format(dim_alias, level.column.lower()))
+                    group_by_count += 1
 
         for measure in self.measures:
             if measure.is_visible:
@@ -117,7 +132,7 @@ class Cube(object):
                 sql_string_columns.append(str_agg_format.format(alias=fact_alias, column=measure.column.lower()))
 
         sql_string_group_order_by = '\nGROUP BY {range}\nORDER BY {range}'.format(range=', '.join([str(i) for i in range(1, group_by_count)]))
-        return 'SELECT\n\t{}\n{}{}\n{}\n;'.format('\n\t, '.join(sql_string_columns), sql_string_from, sql_string_join, sql_string_group_order_by)
+        return 'SELECT\n    {}\n{}{}\n{}\n;'.format('\n    , '.join(sql_string_columns), sql_string_from, sql_string_join, sql_string_group_order_by)
 
 class CalculatedMeasure(object):
     name = ''
@@ -130,7 +145,6 @@ class MondrianFileParser(object):
     __mondrian_file_path = ''
     __dimensions_dict: dict[str, Dimension] = {}
     __cubes_dict: dict[str, Cube] = {}
-
 
     def __init__(self, mondrian_file_path):
         self.__mondrian_file_path = mondrian_file_path
@@ -155,6 +169,7 @@ class MondrianFileParser(object):
                 dim_level.type = lvl_element.get('type', '')
                 dim_level.description = lvl_element.get('description', '')
                 dim_level.column = lvl_element.get('column', '')
+                dim_level.is_visible = lvl_element.get('visible', 'true') == 'true'
                 dim.add_level(dim_level)
 
             self.__dimensions_dict[dim.id] = dim
@@ -203,7 +218,7 @@ class MondrianFileParser(object):
                 dim_usage.foreign_key = dim_usage_element.get('foreignKey', '')
 
                 dim_usage.dimension = self.__dimensions_dict[dim_usage.source]
-                cube.dimensions_usage.append(dim_usage)
+                cube.add_dimension_usage(dim_usage)
 
             for dim_deg in cube_element.iter("Dimension"):
                 dim_usage = DimensionUsage()
@@ -213,7 +228,7 @@ class MondrianFileParser(object):
                 dim_usage.source = cube.table + '.' + dim_deg.get('name')
                 dim_usage.description = dim_deg.get('description', '')
                 dim_usage.dimension = self.__dimensions_dict[dim_usage.source]
-                cube.dimensions_usage.append(dim_usage)
+                cube.add_dimension_usage(dim_usage)
 
             for measure_element in cube_element.iter("Measure"):
                 measure = Measure()
@@ -225,7 +240,7 @@ class MondrianFileParser(object):
                 measure.aggregator = measure_element.get('aggregator', '')
                 measure.is_visible = measure_element.get('visible', 'true') == 'true'
 
-                cube.measures.append(measure)
+                cube.add_measure(measure)
 
             for c_measure_element in cube_element.iter("CalculatedMember"):
                 calculated_measure = CalculatedMeasure()
@@ -234,7 +249,7 @@ class MondrianFileParser(object):
                 calculated_measure.caption = c_measure_element.get('caption', '')
                 calculated_measure.description = c_measure_element.get('description', '')
 
-            self.__cubes_dict[cube.id] = cube
+            self.__cubes_dict[cube.id.lower()] = cube
 
     def parse_file(self):
         file_path = Path(self.__mondrian_file_path)
@@ -247,27 +262,34 @@ class MondrianFileParser(object):
         self.__parse_degenerated_dimensions(document)
         self.__parse_cubes(document)
 
-        for cube_key in self.__cubes_dict:
-            print(cube_key)
-            print(self.__cubes_dict[cube_key].build_sql_select())
+    def build_select_statement_for_cube(self, cube_name: str):
+        return self.__cubes_dict[cube_name.lower()].build_sql_select()
 
 
 def main():   
     parser = argparse.ArgumentParser(
         epilog='Example command: python mondian_doc_gen.py --schema_file "steelwheels.mondrian.xml" --output_dir "output/" --templates_dir "templates_html/" ')
     parser.add_argument('--schema_file', '-s', type=str,
-                        help='The path for the input schema file that will be parsed', required=True)
+                        help='The path for the input schema file that will be parsed', required=False)
+    parser.add_argument('--cube', '-c', type=str,
+                    help='The name of the cube that will be used to generate the SELECT statement.', required=False)
 
-    try:
-        args = parser.parse_args()
-        print(args)
+    # try:
+    args = parser.parse_args()
+    print(args)
 
-        parsed_file = MondrianFileParser(args.schema_file)
+    schema_file = 'redshift_claroca_pt.mondrian.xml' if args.schema_file is None else args.schema_file
+    cube_key = 'voicemail_retrievals_uniques' if args.cube is None else args.cube
+
+    parsed_file = MondrianFileParser(schema_file)
+
+    print(cube_key)
+    print(parsed_file.build_select_statement_for_cube(cube_key))
         
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(e, exc_type, fname, exc_tb.tb_lineno)
+    # except Exception as e:
+    #     exc_type, exc_obj, exc_tb = sys.exc_info()
+    #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    #     print(e, exc_type, fname, exc_tb.tb_lineno)
 
 
 
